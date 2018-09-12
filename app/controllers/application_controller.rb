21ff7ade1ea9ac483f6c7849daab280670ff808d
Ricardo Marin <andresmarin94@hotmail.com>
2018-09-12 22:36:48 +0000

    Finish toy app

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   def hello
-    render html: "hello, world! chapter two"
+    render html: "toy_app"
   end
 end
